bf8812ff7e555c9a61e47e3a501ba3b7518bbe1a
John Graves <johntaylorgraves@gmail.com>
2020-02-04 16:24:28 +0000

    updated !mod command for mysql-promisify

--- commands/mod.js
+++ commands/mod.js
@@ -1,36 +1,31 @@
-/* INFO COMMAND */
+/* MOD COMMAND */
 
-exports.run = async (client, message, args, connection) => {
+// syntax: "!mod @Cepheid"
+// will take the tag of a mentioned user and add them to the database if they are not already a member. Only executable in 'elobot-admin'
+
+exports.run = async (client, message, args, database) => {
 
     if (message.channel.name != 'elobot-admin') return;
 
     let admins = []
 
-    connection.query('SELECT * FROM admins',function(err,results){
-        if (err) throw err;
+    let adminResults = await database.query({sql: `SELECT * FROM admins`})
 
-        for (i in results) {
-            admins.push(results[i].discord_id)
-        }
+    for (i in adminResults.results) {
+        admins.push(adminResults.results[i].discord_id)
+    }
 
-        if (!admins.includes(message.author.tag)) return;
-        
-        if (message.mentions.members.array()[0] === undefined) {
-            message.channel.send('Invalid input')
+    if (!admins.includes(message.author.tag)) return // not an admin
+
+    if (message.mentions.members.array()[0] === undefined) {
+        message.channel.send('Invalid input, please tag a valid user to mod')
+    } else {
+        let user = message.mentions.users.first()
+        if (admins.includes(user.tag)) {
+            message.channel.send('This user is already an admin')
         } else {
-            let user = message.mentions.users.first().tag
-            connection.query('SELECT * FROM admins WHERE discord_id=?',[user], function(err,results) {
-        
-                if (results.length === 0) {
-                    // not in the list of admins
-                    connection.query('INSERT INTO admins (discord_id) VALUES (?)',[user], function(err) {
-                        message.channel.send(`User: ${user} added to the list of admins`)
-                    })
-                } else {
-                    // already an admin
-                    message.channel.send('this user is already an admin')
-                }
-            })
+            database.query({sql: `INSERT INTO admins (discord_id) VALUES ('${user.tag}')`})
+            message.channel.send(`User: ${user} added to the list of admins`)
         }
-    })
+    }
 }
